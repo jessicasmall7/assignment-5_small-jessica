7ad3c4b2387869c6742e8a473f884891acb2b7e9
Jessica-Small <jlsmall@ufl.edu>
2018-07-02 23:17:58 -0400

    listener added

---
+++ js/main.js
@@ -0,0 +1,23 @@
+
+
+
+var title = document.getElementById( 'title' );
+
+title.addEventListener( "click", function() {
+//change H1 text to say 'Tip top!'
+  return "Tip top!";
+});
+
+var paragraph = document.getElementById( 'paragraph' );
+
+paragraph.addEventListener( "mouseover", function() {
+//increase paragraph font size to 20px
+  return fontsize = "20px"
+});
+
+var circle = document.getElementById( 'circle' );
+
+circle.addEventListener( "click", function() {
+//change circle background color to white
+  return circle =  white;
+});
